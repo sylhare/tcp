a8ad6ddb69349cae17a5854b42c5fa1896cea04d
sylhare <sylhare@outlook.com>
2020-04-08 18:41:22 -0400

    Add java socket example

---
+++ src/test/java/greet/GreetTest.java
@@ -0,0 +1,39 @@
+package greet;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GreetTest {
+
+    private static GreetServer server;
+
+    @BeforeAll
+    static void setup(){
+        new Thread(() -> {
+            server = new GreetServer();
+            try {
+                server.start(6666);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        server.stop();
+    }
+
+    @Test
+    void serverRespondsWhenStartedTest() throws IOException {
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        assertEquals("hello client", response);
+    }
+}
