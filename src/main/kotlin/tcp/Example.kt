3e82dcfc4bd556635ba56e76e46506430f344f93
sylhare <sylhare@outlook.com>
2020-04-09 11:53:58 -0400

    Add example

---
+++ src/main/kotlin/tcp/Example.kt
@@ -0,0 +1,48 @@
+package tcp
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import tcp.Example.Companion.client
+import tcp.Example.Companion.server
+import java.net.ServerSocket
+import java.net.Socket
+import java.util.*
+
+
+/**
+ * https://stackoverflow.com/questions/56535473/how-to-send-and-receive-strings-through-tcp-connection-using-kotlin
+ */
+
+class Example {
+
+    companion object {
+        fun server() {
+            val server = ServerSocket(9999)
+            println("Server running on port ${server.localPort}")
+            val client = server.accept()
+            println("Server's client connected : ${client.inetAddress.hostAddress}")
+            val scanner = Scanner(client.inputStream)
+            while (scanner.hasNextLine()) {
+                println("Server received: ${scanner.nextLine()}")
+                break
+            }
+            server.close()
+        }
+
+        fun client() {
+            println("Client ready to stream")
+            val client = Socket("127.0.0.1", 9999)
+            client.outputStream.write("Hello from the client!".toByteArray())
+            client.close()
+        }
+    }
+}
+
+fun main() {
+    GlobalScope.launch {
+        client()
+    }
+    server()
+
+}
+
