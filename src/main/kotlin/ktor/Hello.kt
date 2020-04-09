9f438c4ef1043fb702e1339fd3d81bcdba343faa
sylhare <sylhare@outlook.com>
2020-04-09 08:56:39 -0400

    Add Ktor example

---
+++ src/main/kotlin/ktor/Hello.kt
@@ -0,0 +1,29 @@
+package ktor
+
+
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+/**
+ * https://ktor.io/quickstart/
+ * Access it via http://127.0.0.1:8080
+ */
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
+
