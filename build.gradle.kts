2fdd82ea4e3c4bf634e07ccdc3270807230de3f2
sylhare <sylhare@outlook.com>
2020-04-13 08:42:20 -0400

    Use kotlin DSL for gradle

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "exercise"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    // maven { url "https://dl.bintray.com/kotlin/ktor" }
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor:ktor-server-netty:1.3.2")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("commons-io:commons-io:2.5")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
+    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("io.ktor:ktor-server-test-host:1.3.2")
+    testImplementation("org.mockito:mockito-core:2.23.4")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
