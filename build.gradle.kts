e50f7dc0343b73058b4f49cd977a96a0585e094f
sylhare <sylhare@outlook.com>
2020-04-14 09:40:14 -0400

    Add spring base

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     java
-    kotlin("jvm") version "1.3.61"
+    application
+    `maven-publish`
+    kotlin("jvm") version "1.3.71"
+    kotlin("plugin.spring") version "1.3.71"
+    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
 }
 
 group = "exercise"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+application.mainClassName = "spring.ApplicationKt"
+java.sourceCompatibility = JavaVersion.VERSION_11
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -22,14 +30,19 @@ dependencies {
     testImplementation("io.mockk:mockk:1.9.3")
     testImplementation("io.ktor:ktor-server-test-host:1.3.2")
     testImplementation("org.mockito:mockito-core:2.23.4")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
     }
 }
 
