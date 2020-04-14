f9c112c8e1eaab1f249e4f542c4ca3780eb2a9a0
sylhare <sylhare@outlook.com>
2020-04-14 10:06:38 -0400

    Add java bytes example

---
+++ src/main/java/bytes/Client.java
@@ -0,0 +1,80 @@
+package bytes;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class Client {
+    private Socket socket;
+    private OutputStream out;
+    private InputStream in;
+
+    private Client(String address, int port) {
+        start(address, port);
+        handle();
+        stop();
+    }
+
+    private void handle() {
+        try {
+            byte[] arr = {(byte) 0x00, (byte) 0x01, (byte) 0x00, (byte) 0x10, (byte) 0x00, (byte) 0x01, (byte) 0x00,
+                    (byte) 0x1F, (byte) 0x60, (byte) 0x1D, (byte) 0xA1, (byte) 0x09, (byte) 0x06, (byte) 0x07,
+                    (byte) 0x60, (byte) 0x85, (byte) 0x74, (byte) 0x05, (byte) 0x08, (byte) 0x01, (byte) 0x01,
+                    (byte) 0xBE, (byte) 0x10, (byte) 0x04, (byte) 0x0E, (byte) 0x01, (byte) 0x00, (byte) 0x00,
+                    (byte) 0x00, (byte) 0x06, (byte) 0x5F, (byte) 0x1F, (byte) 0x04, (byte) 0x00, (byte) 0x00,
+                    (byte) 0x18, (byte) 0x1D, (byte) 0xFF, (byte) 0xFF};
+
+            // sending data to server
+            out.write(arr);
+
+            String req = Arrays.toString(arr);
+            //printing request to console
+            System.out.println("Sent to server : " + req);
+
+            // Receiving reply from server
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            baos.write(buffer, 0, in.read(buffer));
+
+            byte[] result = baos.toByteArray();
+
+            String res = Arrays.toString(result);
+
+            // printing reply to console
+            System.out.println("Recieved from server : " + res);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private void stop() {
+        // close the connection
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    private void start(String address, int port) {
+        // establish a connection
+        try {
+            socket = new Socket(address, port);
+            if (socket.isConnected()) {
+                System.out.println("Connected");
+            }
+            // sends output to the socket
+            out = new DataOutputStream(socket.getOutputStream());
+            //takes input from socket
+            in = new DataInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client("127.0.0.1", 5000);
+    }
+}
---
+++ src/main/java/bytes/Server.java
@@ -0,0 +1,75 @@
+package bytes;
+
+// A Java program for a Server
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class Server {
+    //initialize socket and input stream
+    private Socket socket;
+    private ServerSocket server;
+    private InputStream in;
+    private OutputStream out;
+
+    // constructor with port
+    private Server(int port) {
+        // starts server and waits for a connection
+        try {
+            start(port);
+            connectClientSocket();
+            byte[] result = receiveDataFromClient();
+            echoingBackToClient(result);
+            close();
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private void close() throws IOException {
+        // close connection
+        this.socket.close();
+        this.in.close();
+    }
+
+    private void echoingBackToClient(byte[] result) throws IOException {
+        //echoing back to client
+        this.out.write(result);
+        System.out.println("Closing connection");
+    }
+
+    private byte[] receiveDataFromClient() throws IOException {
+        // Receiving data from client
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        baos.write(buffer, 0, in.read(buffer));
+
+        byte[] result = baos.toByteArray();
+
+        String res = Arrays.toString(result);
+        System.out.println("Recieved from client : " + res);
+        return result;
+    }
+
+    private void connectClientSocket() throws IOException {
+        this.socket = server.accept();
+        // takes input from the client socket
+        this.in = new DataInputStream(socket.getInputStream());
+        //writes on client socket
+        this.out = new DataOutputStream(socket.getOutputStream());
+        System.out.println("Client accepted");
+    }
+
+    private void start(int port) throws IOException {
+        server = new ServerSocket(port);
+        System.out.println("Server started");
+        System.out.println("Waiting for a client ...");
+    }
+
+    public static void main(String[] args) {
+        new Server(5000);
+    }
+}
