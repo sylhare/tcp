85d47f9a0ace9c12ef554bc76eb52d918ab52c75
sylhare <sylhare@outlook.com>
2020-04-14 08:26:50 -0400

    Use java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,10 @@ dependencies {
 
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
 }
 
